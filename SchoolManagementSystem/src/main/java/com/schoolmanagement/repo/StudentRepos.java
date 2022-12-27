91b591372a9ac1c2ff38bd1753d4601b8276ea99
Ayushagni01 <ayush.ayush24@gmail.com>
2022-12-27 20:51:45 +0530

    sdc
    student Serviices her e

--- SchoolManagementSystem/src/main/java/com/schoolmanagement/repo/StudentRepos.java
+++
@@ -1,9 +0,0 @@
-package com.schoolmanagement.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;import com.schoolmanagement.model.StudentDetails;
-
-public interface StudentRepos extends JpaRepository<StudentDetails, Long> {
-	
-	
-
-}
